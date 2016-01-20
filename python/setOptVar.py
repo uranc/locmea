9d006aeb73807be3da49b3db0ac9d27493cb19d0
uranc <cemuran@gmail.com>
2016-01-20 19:32:29 +0100

    Initial - minor fix

--- python/setOptVar.py
+++ python/setOptVar.py
@@ -6,7 +6,8 @@ Create attributes for the localization framework
 import numpy as np
 
 
-def create_voxels(elx, ely, elz, p_vres=1, el_radius=5, max_depth=55):
+def create_voxels(elx, ely, elz, p_vres=1, el_radius=5,
+                  max_depth=55, p_jlen=2):
     """
     Create voxel space w.r.t. -m- electrode positions, 1 voxel larger margin
     -------------
@@ -21,16 +22,15 @@ def create_voxels(elx, ely, elz, p_vres=1, el_radius=5, max_depth=55):
     ------------
     vx, vy, vz - Vector valued voxel positions
     """
-    jitter_vector = np.random.random(3)   # iCSD Leski refererence
+    jitter_vector = np.random.random(3) * p_jlen   # iCSD Leski refererence
     n_elx = np.unique(elx).shape[0]
     n_ely = np.unique(ely).shape[0]
     n_elz = np.unique(elz).shape[0]
-    n_el = n_elx*n_ely*n_elz
+    n_el = n_elx * n_ely * n_elz
     # Check position vector formats
     if n_el != elx.shape[0] or n_el != ely.shape[0] \
             or n_el != elz.shape[0] or n_el <= 0:
         print 'Electrode coordinates are wrong'
-    # Find the electrode normal
     el_normal = [n_elx, n_ely, n_elz].index(1)
     # Find min/max values
     min_el = np.ma.array(np.min([elx, ely, elz], 1), mask=False)
@@ -43,31 +43,53 @@ def create_voxels(elx, ely, elz, p_vres=1, el_radius=5, max_depth=55):
     max_el += p_vres + 1
     min_el.mask[el_normal] = False
     max_el.mask[el_normal] = False
-    return np.mgrid[min_el[0]:max_el[0]:p_vres,
-                    min_el[1]:max_el[1]:p_vres,
-                    min_el[2]:max_el[2]:p_vres]
+    return (np.mgrid[min_el[0]:max_el[0]:p_vres, min_el[1]:max_el[1]:p_vres,
+            min_el[2]:max_el[2]:p_vres].T + jitter_vector).T
 
 
-    def cmp_fwd_matrix(elx, ely, elz, vx, vy, vz):
+def cmp_fwd_matrix(elx, ely, elz, vx, vy, vz, p_sigma=0.3):
     """
     Calculate the m-by-n forward matrix given by
     1/(4*pi*sigma)*(1/(d(el_pos-vox_pos)^2)
+    ------------
+    Arguments
+    ------------
+    elx, ely, elz - Vector valued electrode positions
+    vx, vy, vz - Vector valued voxel positions
     """
-    fwd_matrix = np.array(0)
+    n_elx = np.unique(elx).shape[0]
+    n_ely = np.unique(ely).shape[0]
+    n_elz = np.unique(elz).shape[0]
+    n_el = n_elx * n_ely * n_elz
+    n_vx = np.unique(vx).shape[0]
+    n_vy = np.unique(vy).shape[0]
+    n_vz = np.unique(vz).shape[0]
+    n_v = n_vx * n_vy * n_vz
+    # Check position vector formats
+    if n_el != elx.shape[0] or n_el != ely.shape[0] \
+            or n_el != elz.shape[0] or n_el <= 0:
+        print 'Electrode coordinates are wrong'
+    fwd_matrix = np.zeros((n_el, n_v))
+    for el in np.arange(0, n_el):
+        for v in np.arange(0, n_v):
+            fwd_matrix[el, v] = np.sqrt((elx[el] - vx[v])**2 +
+                                        (ely[el] - vy[v])**2 +
+                                        (elz[el] - vz[v])**2)
+    return 1./(fwd_matrix*(4.*np.pi*p_sigma))
 
 
-def cmp_inv_matrix(fwd_matrix, reg_par):
+def cmp_inv_matrix(fwd_matrix, depth_norm_matrix, method='sLoreta'):
     """
-    Computes regularized inverse matrix
+    Computes regularized inverse matrix in the given method
+    (Can be a class later on with multiple methods)
     """
     inv_matrix = np.array(0)
     return inv_matrix
 
 
-def cmp_depth_norm_matrix(fwd_matrix, dpth_par):
+def cmp_depth_norm_matrix(fwd_matrix, p_depth=1.):
     """
     Calculate the column(depth) normalization matrix given by
     (1./sum(a_i^2))^depth_par - column norm for fwd_matrix[:,i]
     """
-    depth_norm_matrix = np.array(0)
-    return depth_norm_matrix
+    return np.diag(np.power(np.sum(fwd_matrix**2, axis=0), 1./(2*p_depth)))
