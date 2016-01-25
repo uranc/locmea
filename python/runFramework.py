b5792652dbaabea82fb317dbb63ad4a7695c37b9
uranc <cemuran@gmail.com>
2016-01-25 18:36:40 +0100

    load simulated hdf5 data

--- python/runFramework.py
+++ python/runFramework.py
@@ -5,7 +5,7 @@ Inverse optimizer example
 # License:
 
 import sys
-from setData import load_data_from_text
+from setData import load_data
 # import numpy as np
 # from newLFPySimulation import newLFPySimulation
 
@@ -34,16 +34,13 @@ def main():
 
     # Data path/filename
     data_path = ''
-    file_name = data_path + 'CAP.csv'
+    file_name = data_path + 'data_sim.hdf5'
+    loaded_data = load_data(file_name)
+    print loaded_data
     print "Looking for file" + file_name
-    data_raw = load_data_from_text(file_name)
-    print data_raw.shape
     # Electrode positions - 3 vectors
     # voxel_pos = create_voxels(elPos[0], elPos[1], elPos
     # [2])
-
-    # Create MNE-Python Raw data format
-    # mne_raw = create_mne_format()
 
 
 if __name__ == "__main__":
--- python/setData.py
+++ python/setData.py
@@ -4,19 +4,54 @@ Module for loading data from various files
 # Author: Cem Uran <cem.uran@uranus.uni-freiburg.de>
 # License:
 import numpy as np
+import h5py
 
 
-def load_data_from_text(f_name):
-    """Load data from txt files"""
-    with open(f_name, 'r+') as f:
-        read_data = f.read()
-    f.closed
-    if read_data:
-        print "Data loaded."
-        print read_data(1)
-    return np.array(read_data)
+class load_data(object):
+    """
+    Data class loaded from file
+    ---------
+    Arguments
+    ---------
+    f_name - filename
+    flag_cell - flag cell morphology information  #ASSUME TRUE
+    ---------
+    Attributes
+    ---------
+    srate
+    flag_pre
+    data
+    ---------
+    Functions
+    ---------
+    """
+    def __init__(self, f_name, flag_cell=True):
+        self.f_name = f_name
+        self.flag_cell = flag_cell
+        print self.f_name
+        if flag_cell:
+            self.cell, self.data_raw, self.srate = self.load_h5py_data(
+                self.f_name, self.flag_cell)
+        else:
+            self.data_raw, self.srate = self.load_h5py_data(self.f_name,
+                                                            self.flag_cell)
 
+    def load_data_from_text(self, f_name):
+        """Load data from txt files"""
+        with open(f_name, 'r+') as f:
+            read_data = f.read()
+        f.closed
+        return np.array(read_data)
 
-# def high_pass_data():
-# def car_data():
-# def epoch_data():
+    def load_h5py_data(self, f_name, flag_cell):
+        f = h5py.File(self.f_name, 'r')
+        print "Cell data avaliable"
+        if flag_cell:
+            return f['cell'][:], f['electrode'][:], f['srate'][()]
+        else:
+            return f['data'][:], f['srate'][()]
+    # def high_pass_data():
+    # def car_data():
+    # def epoch_data():
+    # def cmp_cov_source():
+    # def cmp_cov_meas():
