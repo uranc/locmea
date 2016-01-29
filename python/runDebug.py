f21099b3305667072706bb0c9bff3a4a20ab89c3
uranc <cemuran@gmail.com>
2016-01-29 16:13:56 +0100

    optimization working, temoporary runDebug added, next: visualization - evaluation

--- python/runDebug.py
+++ python/runDebug.py
@@ -6,7 +6,7 @@ Inverse optimizer example
 
 from setData import data_in
 from setOptProb import opt_out
-
+from casadi import *
 
 # Data path/filename
 data_path = ''
@@ -14,3 +14,36 @@ file_name = data_path + 'data_sim_l8e3.hdf5'
 print "Looking for file" + file_name
 data = data_in(file_name, flag_cell=True, flag_electode=False)
 opt = opt_out(data.electrode_pos, p_vres=20, p_jlen=0)
+fwd = SX(opt.cmp_fwd_matrix(opt.electrode_pos, opt.voxels))
+
+
+# Single frame
+y = SX(data.electrode_rec)
+x = SX.sym('x', fwd.shape[1],y.shape[1])
+# Parameters
+
+# Function value
+f = norm_1(x)
+g = (y-mul(fwd, x))**2
+# Initialize
+x0 = [0]
+lbx = [-100]
+ubx = [100]
+lbg = [0]
+ubg = [1e-5]
+# Nonlinear bounds
+
+# Create NLP
+nlp = SXFunction("nlp", nlpIn(x=x), nlpOut(f=f, g=g))
+
+# NLP solver options
+opts = {}
+# opts["compute_red_hessian"] = "yes"
+# Create NLP solver
+solver = NlpSolver("solver", "ipopt", nlp, opts)
+# Solve NLP
+res = solver({"x0": x0,
+              "lbx": lbx,
+              "ubx": ubx,
+              "lbg": lbg,
+              "ubg": ubg})
--- python/runFramework.py
+++ python/runFramework.py
@@ -1,6 +1,19 @@
 """
 Inverse optimizer example
 """
+'''
+Copyright (C) 
+  
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+    
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+'''
 # Author: Cem Uran <cem.uran@uranus.uni-freiburg.de>
 # License:
 
@@ -32,7 +45,6 @@ def main():
     print "Looking for file" + file_name
     data = data_in(file_name, flag_cell=True, flag_electode=False)
     loc = data_out(data.electrode_pos, p_vres=20, p_jlen=0)
-
 
 if __name__ == "__main__":
     sys.exit(main())
