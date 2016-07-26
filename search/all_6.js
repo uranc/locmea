62d6cc9259c98b088b0002425dcd09d8c1666e9a
uranc <cemuran@gmail.com>
2016-07-26 17:28:25 +0200

    multiple time points initialization

--- search/all_6.js
+++ search/all_6.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['g',['g',['../classlocOptimizationProblem_1_1opt__out.html#af0f569092db79158936884251dd64c7c',1,'locOptimizationProblem::opt_out']]],
   ['get_5fground_5ftruth',['get_ground_truth',['../classlocOptimizationProblem_1_1opt__out.html#a567722a4453475dc55834a9fd228bff3',1,'locOptimizationProblem::opt_out']]],
+  ['get_5fkcsd_5festimate',['get_kcsd_estimate',['../classlocOptimizationProblem_1_1opt__out.html#ac3525a672bc25f52d7960b6721324de1',1,'locOptimizationProblem::opt_out']]],
   ['get_5fn_5fin',['get_n_in',['../classlocOptimizationProblem_1_1opt__out_1_1MyCallback.html#a07a90386ff717fd50ec231912a700d26',1,'locOptimizationProblem::opt_out::MyCallback']]],
   ['get_5fn_5fout',['get_n_out',['../classlocOptimizationProblem_1_1opt__out_1_1MyCallback.html#af562d49a1c6fdd4f2570cf7437bd3fd2',1,'locOptimizationProblem::opt_out::MyCallback']]],
   ['get_5fsparsity_5fin',['get_sparsity_in',['../classlocOptimizationProblem_1_1opt__out_1_1MyCallback.html#ac82576d58ed3c6fc749f497eb964cc6e',1,'locOptimizationProblem::opt_out::MyCallback']]]
--- search/variables_12.js
+++ search/variables_12.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['x_5fsize',['x_size',['../classlocOptimizationProblem_1_1opt__out.html#a9da041d0fd354f66350dee0ea69a7a9e',1,'locOptimizationProblem::opt_out']]],
+  ['xreal',['xreal',['../classlocView_1_1visualize.html#aec6ea5da83cd9f4c9a8f6d23e8ec442b',1,'locView::visualize']]],
   ['xres',['xres',['../classlocOptimizationProblem_1_1opt__out.html#ad8c7ddf9ec77cca29d10e9519caf844c',1,'locOptimizationProblem.opt_out.xres()'],['../classlocView_1_1visualize.html#a31ba61235757b6ad0306a5bd22e1bdb9',1,'locView.visualize.xres()']]]
 ];
