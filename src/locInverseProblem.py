8ccf2e81264c6caeb3cbb7d77e9aa75c55ff5944
uranc <cemuran@gmail.com>
2016-07-26 11:20:30 +0200

    rename files, some tutorials

--- src/locInverseProblem.py
+++ src/locInverseProblem.py
@@ -1,5 +1,4 @@
 """
-@package locInverseProblem
 @author Cem Uran <cemuran@gmail.com> 
 Copyright (C) This program is free software: you can redistribute it and/or
 modify it under the terms of the GNU General Public License as published by the
